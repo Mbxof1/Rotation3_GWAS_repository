46269d2d2aed2d09a077d49640325e5ceaad8511
Mbxof1 <mbxof1@nottingham.ac.uk>
2026-04-27 15:52:50 +0100

    Updated names.txt and sample_names.sh

---
+++ scripts/sample_names.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --partition=defq
+#SBATCH --job-name=sample_names
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem=30g
+#SBATCH --time=10:00:00
+#SBATCH --mail-user=[YOUR-EMAIL]
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=fail
+#SBATCH --mail-type=end
+
+#pull all the file names from the raw data directory
+ls ../Data_Folder > file_names.txt
+
+#remove all the contents of the string past the first _
+#increase the value of -f if the sample names contain an _ you want to retain
+cut -d_ -f 1 file_names.txt > names.txt
+
+#removes all duplicates, so only one copy of each sample name is left
+awk -i inplace '!seen[$0]++' names.txt
+
+#remove the original file name document
+rm file_names.txt
